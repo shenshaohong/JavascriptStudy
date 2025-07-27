dfd844acfe6e32ce491d9661fb67ee474192a4ae
shenshaohong <2372192632@qq.com>
2025-07-27 16:32:29 +0800

    webpack引入html自动生成插件

--- webpack_1/01/webpack.config.js
+++ webpack_1/01/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src/login/index.js'),
@@ -6,5 +7,13 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: './login/index.js',
     clean: true //生成打包文件之前，先清除生成目录中的文件
-  }
+  },
+  // 配置插件自动生成html文件插件html- webpack - plugin
+  // 要生成多少个html文件就new多少个HtmlWebpackPlugin()
+  plugins: [new HtmlWebpackPlugin({
+    // 模版来源
+    template: path.resolve(__dirname, './public/login.html'),
+    // 生成文件路径
+    filename: path.resolve(__dirname, 'dist/login/index.html')
+  })],
 }
